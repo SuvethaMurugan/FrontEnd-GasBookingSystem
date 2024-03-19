3372de99f5adb62a95fa8b2e891c0699cb098074
Stalin K M <SSTALINK@ford.com>
2024-03-19 18:31:30 +0530

    Admin

--- src/app/components/deliverylist/deliverylist.component.ts
+++ src/app/components/deliverylist/deliverylist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DeliverylistModule } from '../../model/admin/deliverylist/deliverylist.module';
+import { DeliverylistService } from '../../service/Admin/deliverylist.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-deliverylist',
@@ -8,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: './deliverylist.component.css'
 })
 export class DeliverylistComponent {
-
+  Deliverylist:DeliverylistModule[]=[];
+  getdeliverylist:DeliverylistModule=new DeliverylistModule();
+  constructor(private deliveryservice:DeliverylistService , private router:Router){
+    this.deliveryservice.deliverylistserv().subscribe(
+      {
+        next:(data)=>{
+          console.log(data); 
+        },
+        error:(err)=>{
+          console.log(err.error);
+        },
+        complete:()=>{
+          console.log("Completed data loading");
+        }
+      }
+    )
+  }
 }
